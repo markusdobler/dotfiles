6f39a7d77d1bf1d1093a0c5959c3997511d4943d
Kerstin Sons <stine@sfoo-und-stine.de>
2015-03-08 15:38:27 +0100

    update ambiance plus to work with ubuntu 14.10

--- system_customizations/ambiance-plus.sh
+++ system_customizations/ambiance-plus.sh
@@ -5,6 +5,7 @@ PLUS=${ORIG}-plus
 
 sudo cp -a "${ORIG}" "${PLUS}"
 ( cd "${PLUS}"; sudo patch -p 0 ) < ambiance-plus.patch
+( cd "${PLUS}"; sudo patch -p 0 ) < ambiance-plus-14.10.patch
 
 dconf write /org/gnome/desktop/wm/preferences/theme "'Ambiance-plus'"
 dconf write /org/gnome/desktop/interface/gtk-theme "'Ambiance-plus'"
